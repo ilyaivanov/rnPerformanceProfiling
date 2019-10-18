d62d045e6d5fe5ea51c846746bb8da0d1ea4d714
Ilya <illiai@wix.com>
2019-10-18 16:45:08 +0300

    Create an optimized version of a FlatList

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer, useCallback} from 'react';
 import {FlatList, Image, StatusBar, StyleSheet, Text, View} from 'react-native';
 
 const chunkSize = 300;
@@ -17,41 +17,52 @@ const getColor = index => createColor(index).toString();
 const getTextColor = index => (createColor(index).isDark() ? 'white' : 'black');
 
 const App = () => {
-  const [items, setItems] = useState(Array.from(new Array(chunkSize)));
+  const [items, loadMore] = useItems();
+  const renderer = useCallback(({index}) => <Row index={index} />, []);
 
-  const concat = () => {
-    const newItems = items.concat(Array.from(new Array(chunkSize)));
-    setItems(newItems);
-  };
-
+  // console.log('NEW ITEMS', items.length);
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <View style={{backgroundColor: 'black'}}>
+      <View style={styles.parentView}>
         <FlatList
-          contentContainerStyle={{paddingTop: 20}}
-          onEndReached={concat}
+          contentContainerStyle={styles.container}
+          onEndReached={loadMore}
           onEndReachedThreshold={10}
           data={items}
-          keyExtractor={(_, index) => index + ''}
-          renderItem={({index}) => (
-            <View style={[styles.bar, {backgroundColor: getColor(index)}]}>
-              <Image
-                style={styles.image}
-                height={50}
-                width={50}
-                source={{uri: 'https://cdn141.picsart.com/252986329011202.jpg'}}
-              />
-              <Text style={[styles.text, {color: getTextColor(index)}]}>
-                {Math.floor(index / chunkSize)} - {index + 1}
-              </Text>
-            </View>
-          )}
+          keyExtractor={keyExtractor}
+          renderItem={renderer}
         />
       </View>
     </>
   );
 };
+
+const keyExtractor = (_, index) => index + '';
+
+const createArrayOfSize = size => Array.from(new Array(size));
+
+const loadMore = items => items.concat(createArrayOfSize(chunkSize));
+
+const useItems = () => useReducer(loadMore, createArrayOfSize(chunkSize));
+
+const Row = React.memo(({index}) => {
+  // console.log('Row.render');
+  return (
+    <View style={[styles.bar, {backgroundColor: getColor(index)}]}>
+      <Image
+        style={styles.image}
+        height={50}
+        width={50}
+        source={{uri: 'https://cdn141.picsart.com/252986329011202.jpg'}}
+      />
+      <Text style={[styles.text, {color: getTextColor(index)}]}>
+        {Math.floor(index / chunkSize)} - {index + 1}
+      </Text>
+    </View>
+  );
+});
+
 const styles = StyleSheet.create({
   bar: {
     height: 50,
@@ -67,6 +78,12 @@ const styles = StyleSheet.create({
   text: {
     width: 100,
     fontSize: 23,
+  },
+  container: {
+    paddingTop: 20,
+  },
+  parentView: {
+    backgroundColor: 'black',
   },
 });
 
